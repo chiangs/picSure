a9ab138abfccf9f5219dd98ede14ba393a74875f
DanRezo <DrEzO@DaNReZO.local>
2017-05-03 15:55:34 -0600

    added user field

--- picsure.sql
+++ picsure.sql
@@ -79,6 +79,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` VARCHAR(45) NOT NULL,
   `admin` TINYINT(1) NOT NULL DEFAULT 0,
   `addressId` INT NOT NULL,
+  `picSureEmp` TINYINT(1) NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_user_address1`
     FOREIGN KEY (`addressId`)
@@ -328,8 +329,8 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `picsure`;
-INSERT INTO `user` (`id`, `fName`, `lName`, `username`, `password`, `phone`, `email`, `admin`, `addressId`) VALUES (1, 'Seth', 'Thomas', 'admin', 'p', '555-555-5555', 'swthomas@gmail.com', 1, 2);
-INSERT INTO `user` (`id`, `fName`, `lName`, `username`, `password`, `phone`, `email`, `admin`, `addressId`) VALUES (2, 'Daniel', 'Balarezo', 'danrezo', 'password', '305-484-8911', 'drezo@me.com', 0, 3);
+INSERT INTO `user` (`id`, `fName`, `lName`, `username`, `password`, `phone`, `email`, `admin`, `addressId`, `picSureEmp`) VALUES (1, 'Seth', 'Thomas', 'admin', 'p', '555-555-5555', 'swthomas@gmail.com', 1, 2, NULL);
+INSERT INTO `user` (`id`, `fName`, `lName`, `username`, `password`, `phone`, `email`, `admin`, `addressId`, `picSureEmp`) VALUES (2, 'Daniel', 'Balarezo', 'danrezo', 'password', '305-484-8911', 'drezo@me.com', 0, 3, NULL);
 
 COMMIT;
 
