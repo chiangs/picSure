0d53b69d7c67da3315920021e872534524e9bf19
Developer <stephen.e.chiang@gmail.com>
2017-05-04 12:00:01 -0600

    user basic login/logout show-hide working

--- RESTpicSure/WebContent/ng/app/appModule/app.module.js
+++ RESTpicSure/WebContent/ng/app/appModule/app.module.js
@@ -1,4 +1,4 @@
-angular.module('appModule', [ 'ngRoute', 'static', 'navUser', 'navLister', 'authModule' ]).config(
+angular.module('appModule', [ 'ngRoute', 'static', 'navbar', 'authModule' ]).config(
 		function($routeProvider) {
 			$routeProvider.when('/', {
 				template : '<home></home>'
