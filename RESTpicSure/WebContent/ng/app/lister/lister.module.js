4d9ed9c1c223affd09836a091fa4053fe9acd30b
DanRezo <DrEzO@DaNReZO.local>
2017-05-04 11:46:37 -0600

    skeleton lister modules

---
+++ RESTpicSure/WebContent/ng/app/lister/lister.module.js
@@ -0,0 +1,1 @@
+angular.Module('listerModule', [])
---
+++ RESTpicSure/WebContent/ng/app/lister/lister.service.js
@@ -0,0 +1,8 @@
+angular.Module('authModule')
+	.factory('listerService', function(){
+		var service = {};
+		
+		var BASE_URL = 'http://localhost:8080/RESTpicSure/rest/auth/'
+		
+		
+	})
