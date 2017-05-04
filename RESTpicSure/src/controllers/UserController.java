0ce781e5f82d41c3b20cb6b57d5adfd88b028ecc
Seth Thomas <swthomas34@gmail.com>
2017-05-04 08:57:32 -0600

    fixing dannys shit

--- RESTpicSure/src/controllers/UserController.java
+++ RESTpicSure/src/controllers/UserController.java
@@ -1,5 +1,14 @@
 package controllers;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+import data.UserDAO;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserDAO userDAO;
+	
 }
