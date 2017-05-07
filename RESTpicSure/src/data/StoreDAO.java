31a0c9dab565d305553cb9c9c61abe4787eda625
Seth Thomas <swthomas34@gmail.com>
2017-05-07 15:34:30 -0600

    equipment store query

--- RESTpicSure/src/data/StoreDAO.java
+++ RESTpicSure/src/data/StoreDAO.java
@@ -7,6 +7,7 @@ import entities.Store;
 
 public interface StoreDAO {
 	public List<Store> index();
+	public List<Store> indexEquipment(Integer equipmentId);
 	public Store show(Integer id);
 	public Inventory showInventory(Integer id);
 	public Store update(Integer id, Store s);
