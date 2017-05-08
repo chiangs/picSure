e7a2f97b88599febfe27ba2559748afad150e44e
Seth Thomas <swthomas34@gmail.com>
2017-05-08 08:49:34 -0600

    store, cartItem

--- RESTpicSure/src/data/StoreDAOImpl.java
+++ RESTpicSure/src/data/StoreDAOImpl.java
@@ -32,13 +32,13 @@ public class StoreDAOImpl implements StoreDAO {
 	
 	@Override
 	public List<Store> indexEquipment(Integer equipmentId) {
-		String q = "SELECT s FROM Store s JOIN FETCH Inventory i ON s.id = i.store.id JOIN FETCH InventoryItem ii ON i.id = ii.inventory.id JOIN FETCH Equipment e ON ii.equipment.id = e.id WHERE e.id = :id";
+		String q = "SELECT s FROM Store s JOIN FETCH Inventory i ON s.id = i.store.id JOIN FETCH InventoryItem ii ON i.id = ii.inventory.id JOIN FETCH Equipment e ON ii.equipment.id = e.id WHERE e.id = :id and s.active = true";
 		return em.createQuery(q, Store.class).setParameter("id", equipmentId).getResultList();
 	}
 	
 	@Override
 	public List<Store> index() {
-		String q = "SELECT s FROM Store s";
+		String q = "SELECT s FROM Store s WHERE s.active = true";
 		return em.createQuery(q, Store.class).getResultList();
 	}
 	
