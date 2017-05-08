bd6d106a36797b674dc862f1e5a7849f13d43fd3
Greg Taylor <hesitatetowonder@gmail.com>
2017-05-07 18:20:44 -0600

    geo.service modification

--- RESTpicSure/WebContent/ng/app/lister/geoLocation/geo.service.js
+++ RESTpicSure/WebContent/ng/app/lister/geoLocation/geo.service.js
@@ -6,11 +6,13 @@ angular.module('geoModule')
 			
 		service.address = function(address) {
             var geocoder = new google.maps.Geocoder();
-            var geo = null;
             geocoder.geocode({ 'address': address }, function (results, status) {
                 if (status == google.maps.GeocoderStatus.OK) {
                     var latitude = results[0].geometry.location.lat();
                     var longitude = results[0].geometry.location.lng();
+                    var geo = {longitude:longitude,latitude:latitude};
+                    console.log("longitude "+ geo.longitude+" latitude "+ latitude);
+                    return geo.
                 } else {
                     console.log("Request failed.")
                 }
